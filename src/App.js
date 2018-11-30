2ec3236d493e8a5d680cdd6f3cf44cc25f0abe72
SOF <animeshk55@gmail.com>
2018-11-30 23:24:44 +0530

    key prop not working

--- src/App.js
+++ src/App.js
@@ -5,88 +5,81 @@ import './App.css';
 class App extends Component {
   state = {
     persons: [
-      { name: 'animesh', age: 25 },
-      { name: 'ram', age: 225 },
-      { name: 'rahi,m', age: 275 },
-      { name: 'shyam', age: 295 },
-      { name: 'niishant', age: 2586525 },
-      { name: 'abdul', age: 225 },
+      { id: '25', name: 'animesh', age: 25 },
+      { id: '225', name: 'ram', age: 225 },
+      {  id: '245', name: 'rahi,m', age: 275 },
+      {  id: '255', name: 'shyam', age: 295 },
+      {  id: '5', name: 'niishant', age: 2586525 },
+      {  id: '285', name: 'abdul', age: 225 },
     ]
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked ');
-    this.setState( {
-        persons: [
-          { name: 'this.setState-animesh-this.setState', age: 25 },
-          { name: '.setState-animesh-this.s----ram---.setState-animesh-this.s', age: 225 },
-          { name: 'rahi,m', age: 275 },
-          { name: newName, age: 295 },
-          { name: 'niishant', age: 2586525 },
-          { name: 'abrtfyhnj5drftyuindul', age: 225 },
-          ]
-        }
-      )
-    }
+  // switchNameHandler = (newName) => {
+  //   // console.log('Was clicked ');
+  //   this.setState( {
+  //       persons: [
+  //         { name: 'this.setState-animesh-this.setState', age: 25 },
+  //         { name: '.setState-animesh-this.s----ram---.setState-animesh-this.s', age: 225 },
+  //         { name: 'rahi,m', age: 275 },
+  //         { name: newName, age: 295 },
+  //         { name: 'niishant', age: 2586525 },
+  //         { name: 'abrtfyhnj5drftyuindul', age: 225 },
+  //         ]
+  //       }
+  //     )
+  //   }
 
-    nameChangedHandler = (event) => {
-      this.setState( {
-          persons: [
-            { name: 'this.setState-animesh-this.setState', age: 25 },
-            { name: '.setState-animesh-this.s----ram---.setState-animesh-this.s', age: 225 },
-            { name: 'rahi,m', age: 275 },
-            { name: event.target.value, age: 295 },
-            { name: 'niishant', age: 2586525 },
-            { name: 'abrtfyhnj5drftyuindul', age: 225 },
-            ]
-          }
-        )
-      }
+    // nameChangedHandler = (event) => {
+    //   this.setState( {
+    //       persons: [
+    //         { name: 'this.setState-animesh-this.setState', age: 25 },
+    //         { name: '.setState-animesh-this.s----ram---.setState-animesh-this.s', age: 225 },
+    //         { name: 'rahi,m', age: 275 },
+    //         { name: event.target.value, age: 295 },
+    //         { name: 'niishant', age: 2586525 },
+    //         { name: 'abrtfyhnj5drftyuindul', age: 225 },
+    //         ]
+    //       }
+    //     )
+    //   }
       togglePersonsHandler = () =>  {
         console.log('Was clicked ');
-          const doesShow = this.state.showPerson;
-          this.setState({showPerson: !doesShow});
+          const doesShow = this.state.showPersons;
+          this.setState({showPersons: !doesShow});
+      }
+
+      deletePersonHandler = (personIndex) => {
+          // const persons = this.state.persons;
+          const persons = [...this.state.persons];
+          persons.splice(personIndex, 1);
+          this.setState ({persons : persons })
       }
 
   render() {
     let persons = null;
 
-    if (this.state.showPerson ) {
+    if (this.state.showPersons ) {
       persons = (
         <div>
               <br />
-              <div className="box">
-              <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
-              </div>
-              <div className="box">
-              <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
-              </div>
-              <div className="box">
-              <Person changed={this.nameChangedHandler} name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
-              </div>
-              <div className="box">
-              <Person 
-              name = {this.state.persons[3].name} 
-              age = {this.state.persons[3].age}
-              changed={this.nameChangedHandler}
-              />
-              </div>
-              <div className="box">
-              <Person name = {this.state.persons[4].name} age = {this.state.persons[4].age}>fadsfdsfadffdsfsdfa</Person>
-              </div>
-              <div className="box">
-              <Person 
-              name = {this.state.persons[5].name} 
-              age = {this.state.persons[5].age}
-              click={this.switchNameHandler.bind(this, 'uhiuhifkafjioadsmfsa09jfo')} />
-              </div>
+              {this.state.persons.map((person, index) => {
+                return <div className="box">
+                          <Person
+                          click={() => this.deletePersonHandler(index)}
+                          name={person.name}
+                          age={person.age}
+                          key={person.id} />
+                        </div>
+              })}
           </div>
-      )
+      );
     }
+
     return (
       <div>
           Hello we are working on props
-          <button onClick={this.togglePersonsHandler}>Button</button>
+          <button 
+          onClick={this.togglePersonsHandler}>Button</button>
           {persons}
       </div>
     );
