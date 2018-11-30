c1fe36f28d1cd9d1fa6860b8e0626d798b514496
SOF <animeshk55@gmail.com>
2018-11-30 19:56:01 +0530

    few modifications and alteration in code in inputext and props and state handling

--- src/App.js
+++ src/App.js
@@ -14,26 +14,40 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked ');
     this.setState( {
         persons: [
           { name: 'this.setState-animesh-this.setState', age: 25 },
           { name: '.setState-animesh-this.s----ram---.setState-animesh-this.s', age: 225 },
           { name: 'rahi,m', age: 275 },
-          { name: 'shyam', age: 295 },
+          { name: newName, age: 295 },
           { name: 'niishant', age: 2586525 },
           { name: 'abrtfyhnj5drftyuindul', age: 225 },
-        ]
-      }
-)
+          ]
+        }
+      )
     }
+
+    nameChangedHandler = (event) => {
+      this.setState( {
+          persons: [
+            { name: 'this.setState-animesh-this.setState', age: 25 },
+            { name: '.setState-animesh-this.s----ram---.setState-animesh-this.s', age: 225 },
+            { name: 'rahi,m', age: 275 },
+            { name: event.target.value, age: 295 },
+            { name: 'niishant', age: 2586525 },
+            { name: 'abrtfyhnj5drftyuindul', age: 225 },
+            ]
+          }
+        )
+      }
 
   render() {
     return (
       <div>
           Hello we are working on props
-          <button onClick={this.switchNameHandler }>Button</button>
+          <button onClick={this.switchNameHandler.bind(this, 'Maxfdasffdsnjkm')}>Button</button>
           <br />
           <div className="box">
           <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
@@ -42,10 +56,14 @@ class App extends Component {
           <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
           </div>
           <div className="box">
-          <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
+          <Person changed={this.nameChangedHandler} name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
           </div>
           <div className="box">
-          <Person name = {this.state.persons[3].name} age = {this.state.persons[3].age} />
+          <Person 
+          name = {this.state.persons[3].name} 
+          age = {this.state.persons[3].age}
+          changed={this.nameChangedHandler}
+           />
           </div>
           <div className="box">
           <Person name = {this.state.persons[4].name} age = {this.state.persons[4].age}>fadsfdsfadffdsfsdfa</Person>
@@ -54,7 +72,7 @@ class App extends Component {
           <Person 
           name = {this.state.persons[5].name} 
           age = {this.state.persons[5].age}
-          click={this.switchNameHandler} />
+          click={this.switchNameHandler.bind(this, 'uhiuhifkafjioadsmfsa09jfo')} />
           </div>
       </div>
     );
--- src/Person.js
+++ src/Person.js
@@ -5,6 +5,10 @@ const Person = (props) => {
     <div>
         <p onClick={props.click}>Hello {props.name} your age is {props.age}</p>
         <p>{props.children}</p>
+        <input 
+        type="text"
+        onChange={props.changed}
+        value={props.name} />
     </div>
     )
 }
