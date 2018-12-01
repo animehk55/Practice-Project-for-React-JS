26332dc4553a557c9a50f274142748baa32bc840
SOF <animeshk55@gmail.com>
2018-12-01 22:56:54 +0530

    Added files for push

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+          <Layout>
+              <p>hello </p>
+          </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
